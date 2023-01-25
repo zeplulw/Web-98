31d357ea8c84f7eb22af340f8a9dae13f669962a
zeplulw <74217833+zeplulw@users.noreply.github.com>
2023-01-25 17:54:19 -0500

    fix(main): fix highlighting tool to work

--- js/desktop-highlighting.js
+++ js/desktop-highlighting.js
@@ -1,23 +1,29 @@
-let original_x;
-let original_y;
+let start = {x: 0, y: 0};
+let highlighting_tool = document.querySelector("#highlighting");
 
 document.querySelector("#desktop-apps").onmousedown = e => {
-    original_x = e.clientX;
-    original_y = e.clientY;
-    document.querySelector("#highlighting").style.display = "block";
-    document.querySelector("#highlighting").style.left = original_x + "px";
-    document.querySelector("#highlighting").style.top = original_y + "px";
+    start.x = e.clientX;
+    start.y = e.clientY;
+    highlighting_tool.style.display = "block";
+    highlighting_tool.style.left = start.x + "px";
+    highlighting_tool.style.top = start.y + "px";
 }
 
 document.querySelector("#desktop-apps").onmousemove = e => {
     if (e.buttons == 1) {
-        document.querySelector("#highlighting").style.width = e.clientX - original_x + "px";
-        document.querySelector("#highlighting").style.height = e.clientY - original_y + "px";
+        var min_x = Math.min(start.x, e.clientX);
+        var min_y = Math.min(start.y, e.clientY);
+        var max_x = Math.max(start.x, e.clientX);
+        var max_y = Math.max(start.y, e.clientY);
+        highlighting_tool.style.left = min_x + "px";
+        highlighting_tool.style.top = min_y + "px";
+        highlighting_tool.style.width = (max_x - min_x) + "px";
+        highlighting_tool.style.height = (max_y - min_y) + "px";
     }
 }
 
 document.querySelector("#desktop-apps").onmouseup = e => {
-    document.querySelector("#highlighting").style.display = "none";
-    document.querySelector("#highlighting").style.width = "0px";
-    document.querySelector("#highlighting").style.height = "0px";
+    highlighting_tool.style.display = "none";
+    highlighting_tool.style.width = "0px";
+    highlighting_tool.style.height = "0px";
 }
